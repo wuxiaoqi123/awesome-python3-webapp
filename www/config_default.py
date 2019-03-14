dfea3ebb46f060965d2a3cb5913fe988f774a382
武小奇 <1321972760@qq.com>
2019-03-14 09:19:01 +0800

    day06

---
+++ www/config_default.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+
+'''
+Default configurations
+'''
+
+__author__ = 'wuxiaoqi'
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'wuxiaoqi',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+'''
+Override configurations.
+'''
+
+__author__ = 'wuxiaoqi'
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
