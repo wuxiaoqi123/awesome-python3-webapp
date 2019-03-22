186098b29ef05834d9a6d5cbd0504f26202eb860
武小奇 <1321972760@qq.com>
2019-03-22 10:01:00 +0800

    day-11

--- www/handlers.py
+++ www/handlers.py
@@ -5,19 +5,36 @@ __author__ = 'Michael Liao'
 
 ' url handlers '
 
-import hashlib
-import json
-import logging
-import re
-import time
+import re, time, json, logging, hashlib, base64, asyncio
+
+import markdown2
 
 from aiohttp import web
-from config import configs
+
 from coroweb import get, post
-from models import User, Blog, next_id
+from apis import Page, APIValueError, APIResourceNotFoundError
+
+from models import User, Comment, Blog, next_id
+from config import configs
 
 COOKIE_NAME = 'awesession'
 _COOKIE_KEY = configs.session.secret
+
+
+def check_admin(request):
+    if request.__user__ is None or not request.__user__.admin:
+        raise APIPermissionError()
+
+
+def get_page_index(page_str):
+    p = 1
+    try:
+        p = int(page_str)
+    except ValueError as e:
+        pass
+    if p < 1:
+        p = 1
+    return p
 
 
 def user2cookie(user, max_age):
@@ -149,3 +166,33 @@ async def api_get_users():
     for u in users:
         u.passwd = '******'
     return dict(users=users)
+
+
+@get('/manage/blogs/create')
+def manage_create_blog():
+    return {
+        '__template__': 'manage_blog_edit.html',
+        'id': '',
+        'action': '/api/blogs'
+    }
+
+
+@get('/api/blogs/{id}')
+async def api_get_blog(*, id):
+    blog = await Blog.find(id)
+    return blog
+
+
+@post('/api/blogs')
+async def api_create_blog(request, *, name, summary, content):
+    check_admin(request)
+    if not name or not name.strip():
+        raise APIValueError('name', 'name cannot be empty.')
+    if not summary or not summary.strip():
+        raise APIValueError('summary', 'summary cannot be empty.')
+    if not content or not content.strip():
+        raise APIValueError('content', 'content cannot be empty.')
+    blog = Blog(user_id=request.__user__.id, user_name=request.__user__.name, user_image=request.__user__.image,
+                name=name.strip(), summary=summary.strip(), content=content.strip())
+    await blog.save()
+    return blog
