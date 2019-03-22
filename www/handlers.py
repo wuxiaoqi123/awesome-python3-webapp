bf122d209cc18c51bf8537b4db0f178f6e67651a
武小奇 <1321972760@qq.com>
2019-03-22 09:48:48 +0800

    day10

--- www/handlers.py
+++ www/handlers.py
@@ -5,11 +5,50 @@ __author__ = 'Michael Liao'
 
 ' url handlers '
 
-import re, time, json, logging, hashlib, base64, asyncio
+import hashlib
+import json
+import logging
+import re
+import time
 
+from aiohttp import web
+from config import configs
 from coroweb import get, post
+from models import User, Blog, next_id
 
-from models import User, Comment, Blog, next_id
+COOKIE_NAME = 'awesession'
+_COOKIE_KEY = configs.session.secret
+
+
+def user2cookie(user, max_age):
+    expires = str(int(time.time()) + max_age)
+    s = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
+    L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
+    return '-'.join(L)
+
+
+async def cookie2user(cookie_str):
+    if not cookie_str:
+        return None
+    try:
+        L = cookie_str.split('-')
+        if len(L) != 3:
+            return None
+        uid, expires, sha1 = L
+        if int(expires) < time.time():
+            return None
+        user = await User.find(uid)
+        if user is None:
+            return None
+        s = '%s-%s-%s-%s' % (uid, user.passwd, expires, _COOKIE_KEY)
+        if sha1 != hashlib.sha1(s.encode('utf-8')).hexdigest():
+            logging.info('invalid sha1')
+            return None
+        user.passwd = '******'
+        return user
+    except Exception as e:
+        logging.exception(e)
+        return None
 
 
 @get('/')
@@ -24,6 +63,84 @@ async def index(request):
         '__template__': 'blogs.html',
         'blogs': blogs
     }
+
+
+@get('/register')
+def register():
+    return {
+        '__template__': 'register.html'
+    }
+
+
+@get('/signin')
+def signin():
+    return {
+        '__template__': 'signin.html'
+    }
+
+
+@post('/api/authenticate')
+async def authenticate(*, email, passwd):
+    if not email:
+        raise APIValueError('email', 'Invalid email')
+    if not passwd:
+        raise APIValueError('passwd', 'Invalid password')
+    users = await User.findAll('email=?', [email])
+    if len(users) == 0:
+        raise APIValueError('email', 'Email not exist.')
+    user = users[0]
+    sha1 = hashlib.sha1()
+    sha1.update(user.id.encode('utf-8'))
+    sha1.update(b':')
+    sha1.update(passwd.encode('utf-8'))
+    if user.passwd != sha1.hexdigest():
+        raise APIValueError('passwd', 'Invalid password.')
+    r = web.Response()
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    user.passwd = '******'
+    r.content_type = 'application/json'
+    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+    return r
+
+
+@get('/signout')
+def signout(request):
+    referer = request.headesr.get('Referer')
+    r = web.HTTPFound(referer or '/')
+    r.set_cookie(COOKIE_NAME, '-deleted-', max_age=0, httponly=True)
+    logging.info('user signed out.')
+    return r
+
+
+_RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
+_RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
+
+
+@post('/api/users')
+async def api_register_user(*, email, name, passwd):
+    if not name or not name.strip():
+        raise APIValueError('name')
+    if not email or not _RE_EMAIL.match(email):
+        raise APIValueError('email')
+    if not passwd or not _RE_SHA1.match(passwd):
+        raise APIValueError('passwd')
+        users = await User.findAll('email=?', [email])
+        if len(users) > 0:
+            raise APIError('register:failed', 'email', 'Email is already in use.')
+        uid = next_id()
+        sha1_passwd = '%s:%s' % (uid, passwd)
+        user = User(id=uid, name=name.strip(), email=email,
+                    passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(),
+                    image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(
+                        email.encode('utf-8')).hexdigest())
+        await user.save()
+        # make session cookie:
+        r = web.Response()
+        r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+        user.passwd = '******'
+        r.content_type = 'application/json'
+        r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+        return r
 
 
 @get('/api/users')
